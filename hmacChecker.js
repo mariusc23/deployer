dc6fc786ec5c2981f3b8cceae9396f9e1308af35
Marius Craciunoiu <marius@craciunoiu.net>
2014-09-25 23:10:28 -0400

    Fix hmac checker

--- hmacChecker.js
+++ hmacChecker.js
@@ -1,6 +1,6 @@
 var crypto = require('crypto');
 
 module.exports = function(requestHmac, body, secret) {
-  var generatedHmac = crypto.createHmac('sha1', secret).update(body.toString()).digest('hex');
+  var generatedHmac = crypto.createHmac('sha1', secret).update(JSON.stringify(body)).digest('hex');
   return generatedHmac === requestHmac;
 };
