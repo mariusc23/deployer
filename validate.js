705e3713ba26b13167c104041aa6826a192338c0
Marius Craciunoiu <marius@craciunoiu.net>
2014-09-28 19:43:47 -0400

    Change validation order

--- validate.js
+++ validate.js
@@ -2,21 +2,15 @@ var checkHmac = require('./hmacChecker'),
     fs = require('fs');
 
 module.exports = function (req, res, next) {
-  if (!fs.existsSync(__dirname + '/scripts/' + req.params.site + '.sh')) {
-    var err = new Error('I do not have a deploy script for this site.');
-    err.status = 404;
+  if (!req.headers['x-hub-signature'] || !req.headers['x-hub-signature'].length >= 5) {
+    var err = new Error('You must include x-hub-signature header. See github docs.');
+    err.status = 401;
     return next(err);
   };
 
   if (!process.env[req.params.site.toUpperCase() + '_KEY']) {
     var err = new Error('I do not have a secret for this site.');
     err.status = 404;
-    return next(err);
-  };
-
-  if (!req.headers['x-hub-signature'] || !req.headers['x-hub-signature'].length >= 5) {
-    var err = new Error('You must include x-hub-signature header. See github docs.');
-    err.status = 401;
     return next(err);
   };
 
@@ -28,6 +22,12 @@ module.exports = function (req, res, next) {
     err.status = 403;
     return next(err);
   }
+
+  if (!fs.existsSync(__dirname + '/scripts/' + req.params.site + '.sh')) {
+    var err = new Error('I do not have a deploy script for this site.');
+    err.status = 404;
+    return next(err);
+  };
 
   next();
 };
