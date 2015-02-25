c23d8f489a503490a17343a7d69d01d9b01adc15
Jacob Lindehoff <jacob@cobmedia.se>
2015-02-25 14:56:37 +0100

    Creating Hmac now supports accented characters

--- hmacChecker.js
+++ hmacChecker.js
@@ -1,6 +1,6 @@
 var crypto = require('crypto');
 
 module.exports = function(requestHmac, body, secret) {
-  var generatedHmac = crypto.createHmac('sha1', secret).update(JSON.stringify(body)).digest('hex');
+  var generatedHmac = crypto.createHmac('sha1', secret).update(new Buffer(JSON.stringify(body), 'utf-8')).digest('hex');
   return generatedHmac === requestHmac;
 };
